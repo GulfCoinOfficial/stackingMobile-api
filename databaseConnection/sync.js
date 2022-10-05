db6e84c9bbef33b8de20193657e9d459c7de0276
GulfCoinOfficial <info@gulfofficial.com>
2022-10-05 16:42:07 +0500

    env file issue resolved

--- databaseConnection/sync.js
+++ databaseConnection/sync.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-console */
 require('dotenv').config()
 require("../models/otp")
+require("../models/tokens");
 const sequelize = require('../databaseConnection/db')
 const syncModelsWithDB = async () => {
     try {
